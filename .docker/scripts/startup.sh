290ce1f0e3b31caa07de1d169e35b14e386fd53d
Garrett Davis <garrettd714@gmail.com>
2021-12-18 11:53:52 -0800

    docker: add compose specific files

---
+++ .docker/scripts/startup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Starting startup.sh..."
+echo "Add 10min cron period"
+echo "*/5 * * * * run-parts /etc/periodic/5min" >> /etc/crontabs/root
+crontab -l
+cd /tmt-cli/app
+echo "Copy python streamer to tastyworks package"
+cp lib/tmt_refresh.py /usr/local/lib/python3.9/site-packages/tastyworks/
+echo "Symlinking exes to /usr/local/bin"
+ln -s $PWD/exe/tmt /usr/local/bin/
+ln -s $PWD/exe/tmt-refresh /usr/local/bin/
+echo "Copying settings from example"
+cp settings.yml.example settings.yml
+echo "...end startup.sh"
