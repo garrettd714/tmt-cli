2cae0b3dd6a836657b4a0618b4c1314b841d2441
Garrett Davis <garrettd714@gmail.com>
2021-12-19 16:06:10 -0800

    docker: stop copying settings over on 'up'

--- .docker/scripts/startup.sh
+++ .docker/scripts/startup.sh
@@ -10,7 +10,7 @@ cp lib/tmt_refresh.py /usr/local/lib/python3.9/site-packages/tastyworks/
 echo "Symlinking exes to /usr/local/bin"
 ln -s $PWD/exe/tmt /usr/local/bin/
 ln -s $PWD/exe/tmt-refresh /usr/local/bin/
-echo "Copying settings from example"
-cp settings.yml.example settings.yml
+# echo "Copying settings from example"
+# cp settings.yml.example settings.yml
 echo 'export PS1="\e[1;36m[tmt-cli \T>\e[0m \e[1 q"' >> /root/.bashrc
 echo "...end startup.sh"
